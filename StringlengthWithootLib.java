1d6b0185a46c1807582986a2ed5640b8b0ed0832
hariom yadav <hariom.yadav@apaarinfosystems.com>
2022-10-19 15:40:05 +0530

    add file

---
+++ StringlengthWithootLib.java
@@ -0,0 +1,12 @@
+package package1;
+
+import java.util.Scanner;
+
+public class StringlengthWithootLib {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("pls Enter you are String");
+        String s = sc.nextLine();
+        System.out.println("you are String Length is " + s.length());
+    }
+}
